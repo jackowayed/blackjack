2e5a43aaf6040e3d3cec189a8c34b6b02ea68029
Daniel Jackoway <danjdel@gmail.com>
2011-01-21 21:36:00 -0800

    functional deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,17 @@
 class Deck
+  def initialize
+    reset_deck
+  end
+
+  def draw
+    # reshuffle if the shoe gets too small
+    reset_deck if @cards.length < 26
+
+    @cards.delete_at rand(@cards.length)
+  end
+
+  def reset_deck
+    # shoe of 10 decks
+    @cards = Array(1..13) * 10
+  end
 end
--- test/unit/deck_test.rb
+++ test/unit/deck_test.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 
 require File.join(File.dirname(__FILE__), '../test_helper')
+require 'deck'
 
 class DeckTest < Test::Unit::TestCase
   context "a deck" do
-    
+    setup do
+      @deck = Deck.new
+    end
+
+    context "#draw" do
+      should "be between an Ace and a King" do
+        @card = @deck.draw
+        assert @card >= 0
+        assert @card <= 13
+      end
+
+      should "work an infinite number of times" do
+        1000.times {@deck.draw}
+      end
+    end
   end
 end
