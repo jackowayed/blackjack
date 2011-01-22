c0d4f06341458ff5925c6296c7f6b780d1e4c4da
Daniel Jackoway <danjdel@gmail.com>
2011-01-21 23:01:35 -0800

    change reset_deck to reset since it's in the deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,16 @@
 class Deck
   def initialize
-    reset_deck
+    reset
   end
 
   def draw
     # reshuffle if the shoe gets too small
-    reset_deck if @cards.length < 26
+    reset if @cards.length < 26
 
     @cards.delete_at rand(@cards.length)
   end
 
-  def reset_deck
+  def reset
     # shoe of 10 decks
     @cards = Array(1..13) * 10
   end
